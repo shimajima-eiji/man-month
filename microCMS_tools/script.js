8b1bc25a0ec5a9f01ab2fff3ff62ec9da07b8157
のむら屋ごろう <shimajima-eiji@live.jp>
2020-09-08 04:13:24 +0900

    [2020/09/08][microCMS_tools] JSONに対応

--- microCMS_tools/script.js
+++ microCMS_tools/script.js
@@ -1,3 +1,13 @@
+var CACHE = {
+  url: '',
+  // result: [],
+};
+
+const ids = {
+  url: "url",
+  apikey: "apikey",
+};
+
 function nullcheck ( obj )
 {
   obj.style.background = ( obj.value ) ? "white" : "pink";
@@ -8,35 +18,47 @@ async function run ()
 {
   // 関数
   let id2Obj = ( id ) => document.getElementById( id );
-  const ids = {
-    url: "url",
-    apikey: "apikey",
-  };
 
   // 実行判定
-  const url = nullcheck( id2Obj( ids.url ) );
-  const apikey = nullcheck( id2Obj( ids.apikey ) );
-  if ( !url || !apikey ) return;
+  const URL = nullcheck( id2Obj( ids.url ) );
+  const APIKEY = nullcheck( id2Obj( ids.apikey ) );
+  if ( !URL || !APIKEY ) return;
 
-  const options = {
-    headers: {
-      'X-API-KEY': apikey,
-    }
-  };
+  if ( URL != CACHE.url )
+  {
+    CACHE.url = URL;
+    const options = {
+      headers: {
+        'X-API-KEY': APIKEY,
+      }
+    };
 
-  // curl
-  let result = [];
-  result = await fetch( url + '?limit=100', options )
-    .then( response => response.json() )
-    .then( jsons => jsons.contents );
+    // curl
+    CACHE.result = await fetch( URL + '?limit=100', options )
+      .then( response => response.json() )
+      .then( jsons => jsons.contents );
+  }
 
-  // データ整形
-  const header = Object.keys( result[ 0 ] ).filter( obj =>
+  let response_json = CACHE.result;
+  const header = Object.keys( response_json[ 0 ] ).filter( obj =>
   {
     //"createdAt" "updatedAt" "publishedAt"を削除。デバッグ時はtrueを返す
     return !obj.includes( "At" )
   } );
 
-  result = result.map( json => header.map( index => ( index == "id" ) ? "" : json[ index ] ) );
-  document.form.csv.value = header.join( ',' ) + "\n" + result.join( '\n' );
+  // json
+  document.export.json.value = '';
+  let result = response_json.map( json =>
+  {
+    // json
+    let jsons = {};
+    header.map( id => jsons[ id ] = JSON.stringify( json[ id ] ) );
+    document.export.json.value += JSON.stringify( jsons ) + "\n"
+
+    // csv
+    return header.map( index => json[ index ] );
+  } );
+
+  // csv
+  document.export.csv.value = header.join( ',' ) + "\n" + result.join( '\n' );
 }
