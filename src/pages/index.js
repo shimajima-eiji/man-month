55a4acf7f53d0bb7532ea45ba2e381bb14ac64a0
nomuraya <shimajima-eiji@live.jp>
2021-01-16 15:50:37 +0900

    [2021/01/16] 画像とWebフォントを外して計測

--- src/pages/index.js
+++ src/pages/index.js
@@ -78,7 +78,8 @@ const Style_detail = css`
   }
 
   h1 a{
-    font-family: "corp";
+    /* font-family: "corp"; */
+    font-display: swap;
   }
 `
 const Style_link = css`
@@ -114,7 +115,8 @@ const Style_layout_block = css`
 
   text-align: justify;
   span {
-    font-family: "corp";
+    /* font-family: "corp"; */
+    font-display: swap;
   }
 `
 
@@ -125,7 +127,7 @@ function sumarrize ( html )
 
 export default ( { data } ) =>
 {
-  const noImage = "../images/icon.webp"
+  // const noImage = "../images/icon.webp"
   return (
     <Layout>
       <SEO title="記事一覧" />
@@ -142,7 +144,7 @@ export default ( { data } ) =>
                     <p>{ node.description || sumarrize( node.body ).slice( 0, 100 ) + '......' } </p>
                   </div>
                   <Link to={ node.mainId }><div>
-                    { ( node.door )
+                    {/* { ( node.door )
                       ? <img src={ node.door.url } alt={ node.title } />
                       : <StaticImage
                         src={ noImage }
@@ -150,7 +152,7 @@ export default ( { data } ) =>
                         style={ { width: "200px", height: "200px" } }
                         alt={ node.title }
                       />
-                    }
+                    } */}
                   </div></Link>
                 </div>
                 <div css={ Style_link }><Link to={ node.mainId }>続きを読む　<span>({ node.body.length }文字</span>)</Link></div>
@@ -163,7 +165,7 @@ export default ( { data } ) =>
           <div css={ Style_layout_block }>
             <div>
               <StaticImage
-                src="../images/author.webp"
+                src="../images/author-face.webp"
                 layout="fluid"
                 style={ { width: "128px", height: "128px" } }
                 alt="のむらや"
